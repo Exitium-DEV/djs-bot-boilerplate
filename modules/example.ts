21d04b72d0e512098f1ce2f1e85bdb5e063ea62b
Exitium <gizzib0129+github@gmail.com>
2024-03-30 20:37:44 +0000

    Refactor: Improve module example description

--- modules/example.ts
+++ modules/example.ts
@@ -1,6 +1,6 @@
 import type { Client } from "../utilities/Client";
 
-// MUST BE ASYNC FUNCTION
+// This function will be called when the module is loaded, the name of the function does not matter. It must be async.
 export default async function ExampleModule(client: Client) {
 	console.log("... do something asynchronously");
 }
