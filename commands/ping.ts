dbc92763859107f41a7097e25a59acd4b8efc9b0
Exitium <gizzib0129+github@gmail.com>
2024-03-31 00:54:56 +0000

    Refactor: Improve TS Command Validation

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,13 +1,12 @@
 import {
 	PermissionFlagsBits,
 	SlashCommandBuilder,
-	type ChatInputCommandInteraction,
 } from "discord.js";
 
 import type { Command } from "../types/Command";
 
 export default {
-	permission: async (interaction: ChatInputCommandInteraction) => {							// OPTIONAL, defaults to true, allows custom permission checks
+	permission: async (interaction) => {							// OPTIONAL, defaults to true, allows custom permission checks
 		// do something then return whether the member has permission to run the command
 		return true;
 	},												
@@ -16,7 +15,7 @@ export default {
 		.setDescription("Replies with Pong!")
 		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),							// OPTIONAL, who can see the command?
 
-	execute: async (interaction: ChatInputCommandInteraction) => {								// REQUIRED, what should the command do?
+	execute: async (interaction) => {								// REQUIRED, what should the command do?
 		await interaction.reply("Pong!");
 	},
-} as Command;
+} satisfies Command;
--- types/Command.ts
+++ types/Command.ts
@@ -1,7 +1,7 @@
 import type { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export interface Command {
-	permission?: (interaction?: ChatInputCommandInteraction) => Promise<boolean>;
+	permission?: (interaction: ChatInputCommandInteraction) => Promise<boolean>;
 	data: SlashCommandBuilder;
-	execute: (interaction?: ChatInputCommandInteraction) => Promise<void>;
+	execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
