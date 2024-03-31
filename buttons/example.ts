bdb0c276d6f9870dfdfcec2a03902b421c060d39
Exitium <gizzib0129+github@gmail.com>
2024-03-31 02:03:33 +0000

    Refactor: Button Example Comments

--- buttons/example.ts
+++ buttons/example.ts
@@ -2,11 +2,11 @@ import { type Button } from "../types/Button";
 import { ButtonBuilder, ButtonStyle } from "discord.js";
 
 export default {
-	data: new ButtonBuilder()
+	data: new ButtonBuilder()									// REQUIRED, name is inferred from file name
 		.setLabel("Click me!")
 		.setStyle(ButtonStyle.Primary),
 	
-	execute: async (interaction) => {
+	execute: async (interaction) => {							// REQUIRED, what should the button do?
 		await interaction.reply("Button clicked!");
 	},
 } satisfies Button;
